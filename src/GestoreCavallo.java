0ce87761247f1844edcf9852faf2576dc8320da2
Brugiatig <giuliobrugiati5@gmail.com>
2025-11-17 16:59:48 +0100

    Update GestoreCavallo.java

--- src/GestoreCavallo.java
+++ src/GestoreCavallo.java
@@ -1,32 +1,39 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class GestoreCavallo {
     static String primo="";
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String tmpS;
         int tmp;
         ArrayList<Cavallo> listaCavallo = new ArrayList<Cavallo>();
+
         for (int i = 1; i <= 4; i++) {
             System.out.println("Inserisci il nome del cavallo " + i);
             tmpS =  input.nextLine();
             System.out.println("Inserisci la lentezza del cavallo " + i);
             tmp = input.nextInt();
-            String v = input.nextLine(); //prende il \n
+            String v = input.nextLine();
             Cavallo c=new Cavallo(tmpS, tmp);
             listaCavallo.add(c);
         }
-        int n = (int) (Math.random() /0.2);
+
+        int n = (int) (Math.random() * 4);
         Cavallo x = listaCavallo.get(n);
         x.interrupt();
         listaCavallo.remove(n);
-        
+
         for(Cavallo c: listaCavallo){
             c.start();
         }
-        
+
         System.out.println("Cavallo azzoppato: "+ x.getNomeCavallo());
 
         for(Cavallo c: listaCavallo){
@@ -36,8 +43,9 @@ public class GestoreCavallo {
                throw new RuntimeException(e);
             }
         }
-        System.out.println("Il primo cavallo: " + primo); 
-        //filechooser
+
+        System.out.println("Il primo cavallo: " + primo);
+
         JFileChooser chooser = new JFileChooser();
         chooser.setDialogTitle("Salva il vincitore");
 
@@ -53,9 +61,11 @@ public class GestoreCavallo {
             }
         }
     }
+
     public static String getPrimo() {
         return primo;
     }
+
     public static void setPrimo(String primo) {
         GestoreCavallo.primo = primo;
     }
